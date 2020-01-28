c462c9f292b5b2bbb686bcf96ad27528962af5e0
Rtrojan Nuke <arimiyahmubarak@gmail.com>
2020-01-28 15:23:54 +0100

    Public code method

---
+++ try/src/test/trend.java
@@ -0,0 +1,34 @@
+package test;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class trend {
+	static WebDriver Driver;
+	static String browser;
+	static String projectrail = System.getProperty("user.dir");
+	
+	public static void main(String[] args) {
+		choosebrowser();
+		browsersetting();
+		test();
+	}
+	
+	public static void choosebrowser(){
+		browser = "Chrome";
+	}
+	public static void browsersetting(){
+		if(browser.contains("Chrome")){
+			System.setProperty("webdriver.chrome.driver", projectrail+"\\libraries\\driver\\chromedriver.exe");
+			Driver = new ChromeDriver();
+		}
+		if(browser.contains("Firefox")){
+			System.setProperty("webdriver.firefox.driver", projectrail+"\\libraries\\driver\\chromedriver.exe");
+			Driver = new FirefoxDriver(); 
+		}
+	}
+	public static void test(){
+		Driver.get("konga.com");
+	}
+}
